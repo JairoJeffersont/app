e56981fc384500c7a947884ef3a2844e00725139
Jairo Santos <jairojeffersont@gmail.com>
2024-12-19 16:33:26 -0300

    v1

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -12,7 +12,8 @@ use PDOException;
 /**
  * LoginController
  * 
- * Esta classe gerencia o processo de autenticação de usuários.
+ * Esta classe gerencia o processo de autenticação de usuários no sistema.
+ * Responsável por validar informações de login, gerenciar sessões e controlar o fluxo de recuperação de senha.
  * 
  * @package GabineteDigital\Controllers
  */
@@ -20,43 +21,45 @@ class LoginController {
 
     /**
      * @var UsuarioModel
-     * Instância do controlador de usuários.
+     * Instância do controlador de usuários, utilizado para interagir com os dados do usuário.
      */
     private $usuarioModel;
 
     /**
      * @var array
-     * Configurações do aplicativo.
+     * Configurações gerais do aplicativo, como informações de usuário mestre e configurações de sessão.
      */
     private $config;
 
     /**
      * @var Logger
-     * Instância do logger para registro de eventos.
+     * Instância do logger para registro de eventos, erros e informações importantes.
      */
     private $logger;
 
     /**
      * @var EmailSender
-     * Instância do EmailSender para enviar email de confirmação.
+     * Instância do serviço responsável por enviar emails, como o de recuperação de senha.
      */
     private $emailSender;
 
     /**
      * Construtor do LoginController.
      * 
-     * Inicializa o controlador de usuários, configurações e o logger.
+     * Inicializa o controlador de usuários, configurações e o logger para registro de atividades.
      */
     public function __construct() {
         $this->usuarioModel = new UsuarioModel();
-        $this->config = require './src/Configs/config.php';
-        $this->logger = new Logger('login_log');
+        $this->config = require './src/Configs/config.php'; // Carrega as configurações do aplicativo
+        $this->logger = new Logger('login_log'); // Inicializa o logger para logs de login
         $this->logger->pushHandler(new StreamHandler(dirname(__DIR__, 2) . '/logs/login_log.log', Level::Error));
-        $this->emailSender = new EmailSender();
+        $this->emailSender = new EmailSender(); // Inicializa o serviço de envio de emails
     }
 
     /**
      * Loga um usuário no sistema.
+     * 
+     * Valida o email e a senha do usuário, verificando se o usuário é ativo e retornando informações de sessão.
      * 
      * @param string $email
      * @param string $senha
@@ -64,15 +67,17 @@ class LoginController {
      */
     public function Logar($email, $senha) {
 
+        // Valida o formato do email
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return ['status' => 'invalid_email',  'message' => 'Email inválido.'];
         }
 
+        // Verifica se o login é o usuário mestre configurado
         if ($this->config['master_user']['master_email'] === $email && $this->config['master_user']['master_pass'] === $senha) {
-            session_start();
+            session_start(); // Inicia a sessão
 
             $_SESSION = [
-                'expiracao' => time() + (1 * 60 * 60),
+                'expiracao' => time() + (1 * 60 * 60), // Define a expiração da sessão para 1 hora
                 'usuario_id' => 1,
                 'usuario_nome' => $this->config['master_user']['master_name'],
                 'usuario_nivel' => 0,
@@ -85,22 +90,27 @@ class LoginController {
                 'cliente_assinaturas' => 1,
             ];
 
+            // Loga a informação do usuário mestre
             $this->logger->log(Level::Info, sprintf('%s - %s', date('Y-m-d | H:i'), $this->config['master_user']['master_name']));
 
             return ['status' => 'success', 'message' => 'Usuário verificado com sucesso.'];
         }
 
         try {
+            // Busca o usuário no banco de dados
             $busca = $this->usuarioModel->buscar('usuario_email', $email);
 
+            // Verifica se o usuário foi encontrado
             if (empty($busca)) {
                 return ['status' => 'not_found', 'message' => 'Usuário não encontrado.'];
             }
 
+            // Verifica se o usuário está ativo
             if (!$busca[0]['usuario_ativo']) {
                 return ['status' => 'deactivated', 'message' => 'Usuário desativado.'];
             }
 
+            // Verifica a senha usando a função de verificação do PHP
             if (password_verify($senha, $busca[0]['usuario_senha'])) {
                 session_start();
 
@@ -124,35 +134,39 @@ class LoginController {
                 return ['status' => 'wrong_password', 'message' => 'Senha incorreta.'];
             }
         } catch (PDOException $e) {
-            $erro_id = uniqid();
+            $erro_id = uniqid(); // Gera um identificador único para o erro
             $this->logger->log(Level::Error, $e->getMessage() . ' | ' . $erro_id);
             return ['status' => 'error', 'message' => 'Erro interno do servidor', 'error_id' => $erro_id];
         }
     }
 
-
     /**
-     * Recupera a senha de um usuário, enviando um email de recuperação.
+     * Recupera a senha de um usuário.
+     * 
+     * Envia um email com o token de recuperação de senha se o usuário for encontrado.
      * 
      * @param string $email
      * @return array
      */
     public function recuperarSenha($email) {
         try {
+            // Busca o usuário no banco de dados usando o email
             $busca = $this->usuarioModel->buscar('usuario_email', $email);
 
             if (empty($busca)) {
                 return ['status' => 'not_found', 'message' => 'Usuário não encontrado.'];
             }
 
+            // Gera um token único para recuperação de senha
             $uniqid = uniqid();
-
             $busca[0]['usuario_token'] = $uniqid;
 
+            // Atualiza o token no banco de dados
             $result = $this->usuarioModel->atualizar($busca[0]['usuario_id'], $busca[0]);
 
             if ($result) {
-                $this->emailSender->sendEmail($email, 'Gabinete Digital - Recuperação de senha', 'CORPO DO EMAIL' . $uniqid); //CRIAR CORPO DO EMAIL 
+                // Envia o email de recuperação com o token
+                $this->emailSender->sendEmail($email, 'Gabinete Digital - Recuperação de senha', 'CORPO DO EMAIL' . $uniqid); // Criar corpo do email
                 return ['status' => 'success', 'message' => 'Email de recuperação enviado com sucesso', 'token' => $uniqid];
             }
         } catch (PDOException $e) {
@@ -162,28 +176,29 @@ class LoginController {
         }
     }
 
-
     /**
-     * Gera uma nova senha utilizando o token de recuperação.
+     * Define uma nova senha utilizando o token recebido.
      * 
      * @param string $token
      * @param string $senha
      * @return array
      */
     public function novaSenha($token, $senha) {
-
         try {
+            // Busca o usuário pelo token
             $busca = $this->usuarioModel->buscar('usuario_token', $token);
 
             if (empty($busca)) {
                 return ['status' => 'not_found', 'message' => 'Token inválido.'];
             }
 
+            // Cria o hash da nova senha
             $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
             $busca[0]['usuario_senha'] = $senhaHash;
             $busca[0]['usuario_token'] = null;
 
+            // Atualiza a senha no banco de dados
             $result = $this->usuarioModel->atualizar($busca[0]['usuario_id'], $busca[0]);
 
             if ($result) {
