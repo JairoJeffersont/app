a066e0ff083cb403b342d619927ec1fea636c27e
Jairo Jefferson Teixeira Dos Santos <jairojeffersont@gmail.com>
2024-12-20 12:04:55 -0300

    v1

--- src/Configs/config.php
+++ src/Configs/config.php
@@ -13,9 +13,9 @@ return [
         'master_email' => 'admin@admin.com',
         'master_pass' => 'intell01',
     ],
-
+    
     'app' => [
         'session_time' => 24,
-        'base_url' => rtrim($_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/', '')
+        'base_url' =>rtrim($_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/', '')
     ]
 ];
--- src/Views/erros/fatal_error.php
+++ src/Views/erros/fatal_error.php
@@ -5,5 +5,6 @@
             <img src="public/img/fatal_error.png" alt="Erro 404" style="width: 20em;" class="img-fluid mb-4" />
             <h4 class="text-white">Essa aplicação encontrou um erro fatal e foi encerrada.<br><br>Contate o admistrador do sistema.</h4>
         </div>
+        <a href="?secao=home" class="btn btn-light bt-sm">Tentar novamente</a>
     </div>
 </div>
